f03055e2ec30b15f74145ae513a6abd8f5451043
CodeIce <shenyunjie2008@outlook.com>
2024-05-17 21:36:20 +0800

    create project

--- src/RotorsValue.java
+++ src/RotorsValue.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class RotorsValue {
 
-    private ArrayList<String> RotorsValue = new ArrayList<>();
+    private final ArrayList<String> RotorsValue = new ArrayList<>();
 
     public RotorsValue() {
         RotorsValue.add("EKMFLGDQVZNTOWYHXUSPAIBRCJ");
