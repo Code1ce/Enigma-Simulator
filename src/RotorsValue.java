557d8faf3f6a26f88bec548d4065360289a53a34
CodeIce <shenyunjie2008@outlook.com>
2024-05-17 21:35:40 +0800

    create project

---
+++ src/RotorsValue.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class RotorsValue {
+
+    private ArrayList<String> RotorsValue = new ArrayList<>();
+
+    public RotorsValue() {
+        RotorsValue.add("EKMFLGDQVZNTOWYHXUSPAIBRCJ");
+        RotorsValue.add("AJDKSIRUXBLHWTMCQGZNPYFVOE");
+        RotorsValue.add("BDFHJLCPRTXVZNYEIWGAKMUSQO");
+        RotorsValue.add("ESOVPZJAYQUIRHXLNFTGKDCMWB");
+        RotorsValue.add("VZBRGITYUPSDNHLXAWMJQOFECK");
+        RotorsValue.add("JPGVOUMFYQBENHZRDKASXLICTW");
+        RotorsValue.add("NZJHGRCXMYSWBOUFAIVLPEKQDT");
+        RotorsValue.add("FKQHTLXOCBJSPDZRAMEWNIUYGV");
+        RotorsValue.add("LEYJVCNIXWPBQMDRTAKZGFUHOS");
+        RotorsValue.add("FSOKANUERHMBTIYCWLQPZXVGJD");
+    }
+
+    public ArrayList<String> getRotorsValue() {
+        return RotorsValue;
+    }
+}
